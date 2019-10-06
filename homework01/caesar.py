b58e7d327bea0dab18a2f07cf5101fc093b3968d
e-kondr01 <e.kondr01@gmail.com>
2019-10-06 17:45:58 +0300

    Реализованы функции шифра Цезаря

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -9,7 +9,17 @@ def encrypt_caesar(plaintext: str) -> str:
     >>> encrypt_caesar("")
     ''
     """
-    # PUT YOUR CODE HERE
+    shift = 3
+    ciphertext = ''
+    for char in plaintext:
+        if (ord(char) > 64 and ord(char) < 91 - shift or
+                ord(char) > 96 and ord(char) < 123 - shift):
+            ciphertext += chr(ord(char) + shift)
+        elif (ord(char) > 90 - shift and ord(char) < 91 or
+                ord(char) > 122 - shift and ord(char) < 123):
+            ciphertext += chr(ord(char) - 26 + shift)
+        else:
+            ciphertext += char
     return ciphertext
 
 
@@ -24,5 +34,15 @@ def decrypt_caesar(ciphertext: str) -> str:
     >>> decrypt_caesar("")
     ''
     """
-    # PUT YOUR CODE HERE
+    shift = 3
+    plaintext = ''
+    for char in ciphertext:
+        if (ord(char) > 64 + shift and ord(char) < 91 or
+                ord(char) > 96 + shift and ord(char) < 123):
+            plaintext += chr(ord(char) - shift)
+        elif (ord(char) > 64 and ord(char) < 65 + shift or
+                ord(char) > 96 and ord(char) < 97 + shift):
+            plaintext += chr(ord(char) + 26 - shift)
+        else:
+            plaintext += char
     return plaintext

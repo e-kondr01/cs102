46d14e71f421af0120f9c463bfd8fb2705660163
e-kondr01 <e.kondr01@gmail.com>
2019-12-07 20:58:32 +0300

    разработан поиск сообществ

--- homework05/config.py
+++ homework05/config.py
@@ -1,7 +1,5 @@
 VK_CONFIG = {
     'domain': 'https://api.vk.com/method',
-    'access_token': '''049cbd567\
-e8cd8844e7809632fc43936b03c7c5554\
-8c43cd27a1e5e5f6128f3c08693c5ebb55f73e679b7''',
+    'access_token': '''ec95bd22e164dc567d171e64e40ae83aeef182f298c0732817ef7ed6f4ef6db5e2edecb4d480baaab65c7''',
     'version': '5.103',
 }

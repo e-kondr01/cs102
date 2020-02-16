24ab14d18466f25a931ed112a0bc8552e72c2eef
e-kondr01 <e.kondr01@gmail.com>
2020-02-16 18:22:28 +0300

    сбор данных

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -1,23 +1,42 @@
+import re
 import requests
+
 from bs4 import BeautifulSoup
+from typing import List, Dict
 
 
-def extract_news(parser):
+def extract_news(parser) -> List[Dict]:
     """ Extract news from a given web page """
     news_list = []
-
-    # PUT YOUR CODE HERE
-
+    news_table = parser.table.findAll('table')[1]
+    titles = news_table.findAll('a', class_='storylink')
+    authors = news_table.findAll('a', class_='hnuser')
+    urls = news_table.findAll('a', class_='storylink')
+    points = news_table.findAll('span', class_='score')
+    comments = news_table.find_all('a', string=[re.compile('comment'),
+                                                re.compile('discuss')])
+    for i in range(len(titles)):
+        post_dict = {}
+        post_dict['author'] = authors[i].text
+        post_dict['comments'] = comments[i].text.split()[0]
+        if post_dict['comments'] == 'discuss':
+            post_dict['comments'] = 0
+        post_dict['points'] = points[i].text.split()[0]
+        post_dict['title'] = titles[i].text
+        post_dict['url'] = urls[i].get('href')
+        news_list.append(post_dict)
     return news_list
 
 
-def extract_next_page(parser):
+def extract_next_page(parser) -> str:
     """ Extract next page URL """
-    # PUT YOUR CODE HERE
+    news_table = parser.table.findAll('table')[1]
+    next_page = news_table.find(class_='morelink')['href']
+    return next_page
 
 
-def get_news(url, n_pages=1):
-    """ Collect news from a given web page """
+def get_news(url: str, n_pages: int = 1) -> List:
+    """ Collects news from a given web page """
     news = []
     while n_pages:
         print("Collecting data from page: {}".format(url))
@@ -30,3 +49,7 @@ def get_news(url, n_pages=1):
         n_pages -= 1
     return news
 
+
+if __name__ == '__main__':
+    news_list = get_news("https://news.ycombinator.com/", n_pages=2)
+    print(len(news_list))

1008750282f243baf82898e308e56ced6763eaf1
e-kondr01 <e.kondr01@gmail.com>
2020-02-20 17:23:17 +0300

    add_label() and update_news()

--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -2,7 +2,7 @@ from bottle import (
     route, run, template, request, redirect
 )
 
-from scrapper import get_news
+from scraputils import get_news
 from db import News, session
 from bayes import NaiveBayesClassifier
 
@@ -11,26 +11,46 @@ from bayes import NaiveBayesClassifier
 def news_list():
     s = session()
     rows = s.query(News).filter(News.label == None).all()
-    return template('news_template', rows=rows)
+    return template('C:\cs102\homework06\\news_template.tpl', rows=rows)
 
 
 @route("/add_label/")
 def add_label():
-    # PUT YOUR CODE HERE
+    news_label = request.query.label
+    news_id = request.query.id
+    s = session()
+    changing_news = s.query(News).get(news_id)
+    changing_news.label = news_label
+    s.commit()
     redirect("/news")
 
 
 @route("/update")
 def update_news():
-    # PUT YOUR CODE HERE
+    news_list = get_news("https://news.ycombinator.com/", n_pages=20)
+    s = session()
+    old_news_list = s.query(News).all()
+    for news in news_list:
+        for old_news in old_news_list:
+            if (news['title'] == old_news.title and
+                    news['author'] == old_news.author):
+                break
+        else:
+            new_news = News(title=news['title'],
+                            author=news['author'],
+                            url=news['url'],
+                            comments=news['comments'],
+                            points=news['points'])
+            s.add(new_news)
+            s.commit()
     redirect("/news")
 
 
 @route("/classify")
 def classify_news():
     # PUT YOUR CODE HERE
+    pass
 
 
 if __name__ == "__main__":
     run(host="localhost", port=8080)
-

fff2cd5e2360c55b511c336a65dd1adc5fe85534
e-kondr01 <e.kondr01@gmail.com>
2020-04-17 20:59:10 +0300

    lab7 v0.2

--- homework07/async_http.py
+++ homework07/async_http.py
@@ -5,9 +5,10 @@ import multiprocessing
 import logging
 import mimetypes
 import os
-from urlparse import parse_qs
 import urllib
 import argparse
+
+from datetime import datetime
 from time import strftime, gmtime
 
 
@@ -52,7 +53,8 @@ class AsyncServer(asyncore.dispatcher):
         self.listen(5)
 
     def handle_accepted(self, sock, addr):
-        log.debug(f"Incoming connection from {addr}")
+        #  log.debug(f"Incoming connection from {addr}")
+        print(f"Incoming connection from {addr}")
         AsyncHTTPRequestHandler(sock)
 
     def serve_forever(self):
@@ -63,32 +65,55 @@ class AsyncHTTPRequestHandler(asynchat.async_chat):
 
     def __init__(self, sock):
         super().__init__(sock)
+        self.ibuffer = []
+        self.reading_headers = True
+        self.handling = False
         self.set_terminator(b"\r\n\r\n")
+        self.curr_request = ''
+        self.method = 'undefined'
+        self.headers = {}
 
     def collect_incoming_data(self, data):
-        log.debug(f"Incoming data: {data}")
-        self._collect_incoming_data(data)
+        #  log.debug(f"Incoming data: {data}")
+        print(f"Incoming data: {data}")
+        self.ibuffer.append(data.decode('ASCII'))
 
     def found_terminator(self):
-        self.parse_request()
+        if self.reading_headers:
+            self.reading_headers = False
+            self.parse_headers()
+            self.ibuffer = []
+            self.handling = True
+            self.parse_request()
+        elif self.handling:
+            self.parse_request()
 
     def parse_request(self):
-        pass
+        self.handle_request()
 
     def parse_headers(self):
-        pass
+        unparsed = "".join(self.ibuffer)
+        headers_lst = unparsed.split('\r\n')
+        if 'HEAD' in headers_lst[0]:
+            self.method = 'HEAD'
+        elif 'GET' in headers_lst[0]:
+            self.method = 'GET'
+        for header in headers_lst[1:]:
+            key, value = header.split(': ')
+            self.headers[key] = value
+        print('Headers parsed')
 
     def handle_request(self):
-        method_name = 'do_' + self.method
-        if not hasattr(self, method_name):
+        if self.method == 'GET':
+            self.do_GET()
+        elif self.method == 'HEAD':
+            self.do_HEAD()
+        else:
             self.send_error(405)
             self.handle_close()
-            return
-        handler = getattr(self, method_name)
-        handler()
 
     def send_header(self, keyword, value):
-        pass
+        self.push(f'{keyword}: {value}\r\n'.encode())
 
     def send_error(self, code, message=None):
         try:
@@ -103,26 +128,35 @@ class AsyncHTTPRequestHandler(asynchat.async_chat):
         self.send_header("Connection", "close")
         self.end_headers()
 
-    def send_response(self, code, message=None):
-        pass
+    def send_response(self, code: int, message=None):
+        self.push(f'HTTP/1.1 {code} {responses[code]}\r\n'.encode())
 
     def end_headers(self):
-        pass
+        self.push(f'\r\n'.encode())
 
     def date_time_string(self):
         pass
 
     def send_head(self):
-        pass
+        curr_time = datetime.now().time()
+        self.send_header('Server', 'async_http_py_Kondrashov')
+        self.send_header('Date', curr_time)
+        self.end_headers()
 
     def translate_path(self, path):
         pass
 
     def do_GET(self):
-        pass
+        self.send_response(200)
+        self.send_head()
+        self.handle_close()
+        print('GET is done')
 
     def do_HEAD(self):
-        pass
+        self.send_response(200)
+        self.send_head()
+        self.handle_close()
+        print('HEAD is done')
 
 
 responses = {
@@ -142,13 +176,13 @@ def parse_args():
     parser.add_argument("--host", dest="host", default="127.0.0.1")
     parser.add_argument("--port", dest="port", type=int, default=9000)
     parser.add_argument("--log", dest="loglevel", default="info")
-    parser.add_argument("--logfile", dest="logfile", default=None)
+    parser.add_argument("--logfile", dest="logfile", default="server_logs.txt")
     parser.add_argument("-w", dest="nworkers", type=int, default=1)
     parser.add_argument("-r", dest="document_root", default=".")
     return parser.parse_args()
 
 
-def run():
+def run(args):
     server = AsyncServer(host=args.host, port=args.port)
     server.serve_forever()
 
@@ -156,13 +190,13 @@ def run():
 if __name__ == "__main__":
     args = parse_args()
 
-    logging.basicConfig(
+    ''' logging.basicConfig(
         filename=args.logfile,
         level=getattr(logging, args.loglevel.upper()),
         format="%(name)s: %(process)d %(message)s")
-    log = logging.getLogger(__name__)
+    log = logging.getLogger(__name__) '''
 
     DOCUMENT_ROOT = args.document_root
-    for _ in xrange(args.nworkers):
-        p = multiprocessing.Process(target=run)
+    for _ in range(args.nworkers):
+        p = multiprocessing.Process(target=run(args))
         p.start()

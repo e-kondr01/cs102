4880d5b2905399684b808d83e025ad67e600482d
e-kondr01 <e.kondr01@gmail.com>
2020-06-15 17:12:50 +0300

    let's see if it can be done with default pool

--- homework10/main.py
+++ homework10/main.py
@@ -2,9 +2,19 @@ from pools import ProcessPool
 
 
 def heavy_computation(data_chunk):
-    pass
+    print(sum(data_chunk)**2)
+
+
+def get_big_data():
+    big_data = []
+    big_data.append([i for i in range(1000)])
+    big_data.append([i for i in range(1001)])
+    big_data.append([i for i in range(1002)])
+    big_data.append([i for i in range(1003)])
+    return big_data
 
 
 if __name__ == '__main__':
+    big_data = get_big_data()
     pool = ProcessPool(min_workers=2, max_workers=10, mem_usage='1Gb')
-    #  results = pool.map(heavy_computation, big_data)
+    results = pool.map(heavy_computation, big_data)
--- homework10/pools.py
+++ homework10/pools.py
@@ -1,20 +1,43 @@
 import multiprocessing
+import psutil
+import time
 
 
 class ProcessPool:
     def __init__(self, min_workers, max_workers, mem_usage):
         self.min_workers = min_workers
         self.max_workers = max_workers
-        self.mem_usage = self.mem_in_bits(mem_usage)
-        self.workers = []
+        self.mem_usage = self.mem_in_bytes(mem_usage)
 
-        self.in_queue = multiprocessing.Queue
-        self.out_queue = multiprocessing.Queue
-
-    def mem_in_bits(self, s):
+    def mem_in_bytes(self, s):
         if 'Gb' in s:
-            return int(s[:len(s)-2]) * 2 ** 33
+            return int(s[:len(s)-2]) * 2 ** 30
         elif 'Mb' in s:
-            return int(s[len(s)-2]) * 2 ** 23
+            return int(s[:len(s)-2]) * 2 ** 20
         else:
             raise Exception('Invalid memory format')
+
+    def map(self, func, data):
+        self.guess_max_workers(func, data[0])
+        if self.max_workers < self.min_workers:
+            raise Exception('Not enough memory to have min workers')
+        print(f'Looks like there should be {self.max_workers} workers')
+        self.pool = multiprocessing.Pool(processes=self.max_workers)
+        res = self.pool.map_async(func, data[1:])
+        res.get()
+        self.pool.close()
+
+    def guess_max_workers(self, func, data):
+        data = [data]  # check in another dataset
+        p = multiprocessing.Process(target=func, args=data)
+        p.start()
+        memory_usage = []
+        while p.is_alive():
+            try:
+                memory_usage.append(psutil.Process(p.pid).memory_info()[0])
+                time.sleep(0.01)
+            except psutil.NoSuchProcess:
+                break
+        peak_memory_usage = max(memory_usage)
+        self.max_workers = min(self.mem_usage // peak_memory_usage,
+                               self.max_workers)
